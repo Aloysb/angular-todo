89839c4d8ef59be63a744e637e47833b07bc16f1
Aloys Berger <alo@reaktive.com>
2022-09-27 22:47:36 +1000

    e2e: write first 'outside' e2e test

--- cypress/e2e/spec.cy.ts
+++ cypress/e2e/spec.cy.ts
@@ -1,6 +1,19 @@
-describe('My First Test', () => {
-  it('Visits the initial project page', () => {
-    cy.visit('/')
-    cy.contains('app is running!')
+import {environment} from "../../src/environments/environment";
+
+describe('Todo list', () => {
+  it('displays the list of existing todo when I visist the site', () => {
+    // TODO: where to put interfaces?
+    interface Todo {id: number, description:string, status: 'done'|'todo'};
+
+    const getTodosUrl = environment.api.getTodos;
+    const todos:Todo[] = [
+      {id: 1, description: "Clean the kitchen", status: "done"},
+      {id: 2, description: "Relax", status: "todo"}
+    ]
+    cy.intercept('GET', 'api/todos', todos);
+    cy.visit('/');
+    todos.forEach(({description}) => {
+      cy.contains(description);
+    })
   })
 })
